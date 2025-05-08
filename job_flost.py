26e3ac8e0928c5ce5de9bdde4bc447ad82e04047
Jiuqian Shang <jiuqian@stat-jiuqian-mba.local>
2025-05-08 14:32:37 -0400

    Initial commit

---
+++ job_flost.py
@@ -0,0 +1,47 @@
+# dependencies python3
+import numpy as np
+from scipy.io import loadmat
+import math
+import random
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import QuantileTransformer
+#from scipy.special import boxcox, inv_boxcox
+from scipy.linalg import svd
+from scipy.ndimage import uniform_filter, maximum_filter, gaussian_filter
+# visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+from bayes_opt import BayesianOptimization
+
+# my functions 
+from generate_data import generate_tensor_vista, generate_tensor_simulated
+from preprocess_data import randomly_drop_pixels, get_singular_values,robust_scaler, inverse_robust_scaler
+from completion_alg import sparse_low_rank
+from smoothing import smooth_tensor_mean_filter, smooth_tensor_gaussian_filter, smooth_tensor_maximum_filter
+from check_results import error_plot, compare_maps
+
+from TEC_class import TEC_data
+
+import sys
+k = int(sys.argv[1])
+
+dates = ['190621', '190622', '190623', '190624', '190625', '190626']
+
+VISTA_1906 = TEC_data(dates, preprocess=False,r=5, k=k, missing_rate = 0.5, cutted = True)
+VISTA_1906.tune_lam2(lower=50, upper=3000, lam1=VISTA_1906.sv2, metric = 'rmse',
+                  init_points=10, n_iter=50,verbose = 0,random_state=0)
+VISTA_1906.tune_lam1(VISTA_1906.lam2_opt, metric = 'rmse',
+                         init_points=5, n_iter=50,verbose = 0,random_state=0,)
+lam_results = VISTA_1906.lam2_opt + VISTA_1906.lam1_opt
+
+VISTA_1906 = TEC_data(dates, preprocess='quantile',r=5, k=k, missing_rate = 0.5, cutted = True)
+VISTA_1906.tune_lam2(lower=50, upper=2000, lam1=VISTA_1906.sv2, metric = 'rmse',
+                  init_points=10, n_iter=50,verbose = 0,random_state=0)
+VISTA_1906.tune_lam1(VISTA_1906.lam2_opt, metric = 'rmse',
+                         init_points=5, n_iter=50,verbose = 0,random_state=0,)
+lam_results_quantile = VISTA_1906.lam2_opt + VISTA_1906.lam1_opt
+
+# 2. JSON (also human‑readable, keeps Python list structure)
+import json, pathlib
+pathlib.Path(f"lams_{k}.json").write_text(json.dumps({"lam_results": lam_results, "lam_results_quantile": lam_results_quantile}))
+
