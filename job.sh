ae6c8aabb10f8dcb733e1130a0b1a6e01634fe0b
Jiuqian Shang <jiuqian@stat-jiuqian-mba.local>
2025-05-08 14:36:14 -0400

    Initial commit

---
+++ job.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+## sbatch job.sh to run
+#SBATCH --job-name=FLoST
+#SBATCH --mail-user=jiuqian@umich.edu
+#SBATCH --mail-type=FAIL,ARRAY_TASKS
+
+#SBATCH --account=stats_dept1
+#SBATCH --partition=standard
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+
+## 5GB/cpu is the basic share
+#SBATCH --mem-per-cpu=10GB
+
+## wall time hours:minutes:seconds
+#SBATCH --time=10:00:00
+#SBATCH --array=10, 50, 100, 200, 300, 400
+
+###   Load software modules
+####  Commands your job should run follow this line
+###   Load software modules
+module load python3.11-anaconda/2024.02
+####  Commands your job should run follow this line
+python3 job_flost.py $SLURM_ARRAY_TASK_ID 
