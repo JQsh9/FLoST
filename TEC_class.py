a9416b447817533f9b6a4e8b0b704b93311aa601
Jiuqian Shang <jiuqian@stat-jiuqian-mba.local>
2025-05-09 00:33:04 -0400

    validation tune

--- TEC_class.py
+++ TEC_class.py
@@ -47,13 +47,17 @@ class TEC_data():
             self.qt = QuantileTransformer(n_quantiles=n_quantiles, output_distribution='normal', random_state=2025)
             flattern_sorted = self.qt.fit_transform(flattern_sorted)
             self.tensor_dropped = flattern_sorted.reshape(self.tensor_dropped.shape)
+        self.tensor_train, self.val_idx= holdout_validation(self.tensor_dropped, val_ratio=0.10, seed=0)
         
         self.k, self.r = k, r
         self.sv1, self.sv2 = get_singular_values(self.tensor_dropped,self.r,self.k)
 
     
-    def run_completion(self,lam1,lam2):
-        self.tensor_hat = sparse_low_rank(self.tensor_dropped, self.missing_rate, self.k, lam1, lam2)
+    def run_completion(self,lam1,lam2, train=False):
+        if train:
+            self.tensor_hat = sparse_low_rank(self.tensor_train, self.missing_rate, self.k, lam1, lam2)
+        else:
+            self.tensor_hat = sparse_low_rank(self.tensor_dropped, self.missing_rate, self.k, lam1, lam2)
         # inverse processing
         if self.preprocess == 'log':
             self.tensor_hat = np.exp(self.tensor_hat)
@@ -63,6 +67,7 @@ class TEC_data():
             shape = self.tensor_hat.shape
             self.tensor_hat = self.qt.inverse_transform(self.tensor_hat.reshape(-1, 1)).reshape(shape)
         else: pass
+
 
     def tune_lam2(self, 
                   lower, upper, lam1, metric = 'rmse',
@@ -119,6 +124,72 @@ class TEC_data():
                     return -np.linalg.norm(self.tensor_hat - self.tec_VISTA)/np.linalg.norm(self.tec_VISTA)
                 elif metric == 'rmse':
                     return -np.linalg.norm(self.tensor_hat - self.tec_VISTA)/math.sqrt(self.tec_VISTA.size)
+            pbounds = {'x': (self.sv2[i-2]-1000, self.sv1[i-2]+1000), 
+                       'y': (self.sv2[i-1]-1000, self.sv1[i-1]+1000)}
+            optimizer = BayesianOptimization(
+                f=black_box_function,
+                pbounds=pbounds,random_state=random_state,verbose=verbose,)
+            optimizer.maximize(init_points=init_points,n_iter=n_iter)
+            tuned.append(optimizer.max['params']['x'])
+            tuned.append(optimizer.max['params']['y'])
+        self.lam1_opt = tuned
+        self.error = optimizer.max['target']
+
+    def validation_tune_lam2(self, 
+                  lower, upper, lam1, metric = 'rmse',
+                  init_points=10, n_iter=50,
+                  verbose = 2,random_state=0):  
+        def black_box_function(x,y):
+            self.run_completion(lam1=lam1, lam2=[x,y], train=True)
+            if metric == 'rse':
+                return -np.linalg.norm(self.tensor_hat[self.val_idx]-self.tensor_dropped[self.val_idx])/np.linalg.norm(self.tensor_dropped[self.val_idx])
+            elif metric == 'rmse':
+                return -np.linalg.norm(self.tensor_hat[self.val_idx] - self.tensor_dropped[self.val_idx])/math.sqrt(self.tensor_hat.size)
+        pbounds = {'x': (lower, upper), 'y': (lower, upper)}
+        optimizer = BayesianOptimization(
+            f=black_box_function,
+            pbounds=pbounds,random_state=random_state,verbose=verbose,)
+        optimizer.maximize(init_points=init_points,n_iter=n_iter)
+        self.lam2_opt = [optimizer.max['params']['x'], optimizer.max['params']['y']]
+        self.error= optimizer.max['target']
+
+    def validation_tune_lam1_parallel(self, tune_start, n_tune,
+                  lam2, metric = 'rmse',
+                  init_points=5, n_iter=50,
+                  verbose = 0,random_state=0,):
+        tuned = []
+        for i in range(tune_start, tune_start+n_tune, 2):
+            def black_box_function(x,y):
+                lam1_tune = tuned + [x,y] + self.sv2[i:]
+                self.run_completion(lam1=lam1_tune, lam2=lam2,train=True)
+                if metric == 'rse':
+                    return -np.linalg.norm(self.tensor_hat[self.val_idx]-self.tensor_dropped[self.val_idx])/np.linalg.norm(self.tensor_dropped[self.val_idx])
+                elif metric == 'rmse':
+                    return -np.linalg.norm(self.tensor_hat[self.val_idx] - self.tensor_dropped[self.val_idx])/math.sqrt(self.tensor_hat.size)
+            pbounds = {'x': (self.sv2[i-2]-1000, self.sv1[i-2]+1000), 
+                       'y': (self.sv2[i-1]-1000, self.sv1[i-1]+1000)}
+            optimizer = BayesianOptimization(
+                f=black_box_function,
+                pbounds=pbounds,random_state=random_state,verbose=verbose,)
+            optimizer.maximize(init_points=init_points,n_iter=n_iter)
+            tuned.append(optimizer.max['params']['x'])
+            tuned.append(optimizer.max['params']['y'])
+        self.lam1_opt = tuned
+        self.error = optimizer.max['target']
+
+    def validation_tune_lam1(self,
+                  lam2, metric = 'rmse',
+                  init_points=5, n_iter=50,
+                  verbose = 0,random_state=0,):
+        tuned = []
+        for i in range(2, self.k+1, 2):
+            def black_box_function(x,y):
+                lam1_tune = tuned + [x,y] + self.sv2[i:]
+                self.run_completion(lam1=lam1_tune, lam2=lam2,train=)
+                if metric == 'rse':
+                    return -np.linalg.norm(self.tensor_hat[self.val_idx]-self.tensor_dropped[self.val_idx])/np.linalg.norm(self.tensor_dropped[self.val_idx])
+                elif metric == 'rmse':
+                    return -np.linalg.norm(self.tensor_hat[self.val_idx] - self.tensor_dropped[self.val_idx])/math.sqrt(self.tensor_hat.size)
             pbounds = {'x': (self.sv2[i-2]-1000, self.sv1[i-2]+1000), 
                        'y': (self.sv2[i-1]-1000, self.sv1[i-1]+1000)}
             optimizer = BayesianOptimization(
