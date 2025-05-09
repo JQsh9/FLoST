d95e79b182f67028bc7f46692f3d2a45b47367d4
Jiuqian Shang <jiuqian@stat-jiuqian-mba.local>
2025-05-08 23:07:15 -0400

    k=200

--- job_flost.py
+++ job_flost.py
@@ -23,25 +23,31 @@ from check_results import error_plot, compare_maps
 from TEC_class import TEC_data
 
 import sys
-k = int(sys.argv[1])
+tune_start = int(sys.argv[1])
 
 dates = ['190621', '190622', '190623', '190624', '190625', '190626']
 
 VISTA_1906 = TEC_data(dates, preprocess=False,r=5, k=k, missing_rate = 0.5, cutted = True)
 VISTA_1906.tune_lam2(lower=50, upper=3000, lam1=VISTA_1906.sv2, metric = 'rmse',
                   init_points=10, n_iter=50,verbose = 0,random_state=0)
-VISTA_1906.tune_lam1(VISTA_1906.lam2_opt, metric = 'rmse',
-                         init_points=5, n_iter=50,verbose = 0,random_state=0,)
-lam_results = VISTA_1906.lam2_opt + VISTA_1906.lam1_opt
+VISTA_1906.tune_lam1_parallel(tune_start, 50,
+                                VISTA_1906.lam2_opt, metric = 'rmse',
+                                init_points=5, n_iter=50,verbose = 0,random_state=0,)
+if tune_start == 2:
+    lam_results = VISTA_1906.lam2_opt + VISTA_1906.lam1_opt
+else: lam_results = VISTA_1906.lam1_opt
 
 VISTA_1906 = TEC_data(dates, preprocess='quantile',r=5, k=k, missing_rate = 0.5, cutted = True)
 VISTA_1906.tune_lam2(lower=50, upper=2000, lam1=VISTA_1906.sv2, metric = 'rmse',
                   init_points=10, n_iter=50,verbose = 0,random_state=0)
-VISTA_1906.tune_lam1(VISTA_1906.lam2_opt, metric = 'rmse',
-                         init_points=5, n_iter=50,verbose = 0,random_state=0,)
-lam_results_quantile = VISTA_1906.lam2_opt + VISTA_1906.lam1_opt
+VISTA_1906.tune_lam1_parallel(tune_start, 50,
+                                VISTA_1906.lam2_opt, metric = 'rmse',
+                                init_points=5, n_iter=50,verbose = 0,random_state=0,)
+if tune_start == 2:
+    lam_results_quantile = VISTA_1906.lam2_opt + VISTA_1906.lam1_opt
+else: lam_results_quantile = VISTA_1906.lam1_opt
 
 # 2. JSON (also human‑readable, keeps Python list structure)
 import json, pathlib
-pathlib.Path(f"lams_{k}.json").write_text(json.dumps({"lam_results": lam_results, "lam_results_quantile": lam_results_quantile}))
+pathlib.Path(f"lams_200_{tune_start}.json").write_text(json.dumps({"lam_results": lam_results, "lam_results_quantile": lam_results_quantile}))
 
