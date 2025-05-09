f8439acc131e5b1048dc8e44739abbcad9124b06
Jiuqian Shang <jiuqian@stat-jiuqian-mba.local>
2025-05-09 01:20:38 -0400

    val tune 100

--- job_flost.py
+++ job_flost.py
@@ -25,14 +25,14 @@ from TEC_class import TEC_data
 import sys
 #k = int(sys.argv[1])
 tune_start = int(sys.argv[1])
-k=400
+k=100
 
 dates = ['190621', '190622', '190623', '190624', '190625', '190626']
 
 VISTA_1906 = TEC_data(dates, preprocess=False,r=5, k=k, missing_rate = 0.5, cutted = True)
-VISTA_1906.tune_lam2(lower=50, upper=3000, lam1=VISTA_1906.sv2, metric = 'rmse',
+VISTA_1906.validation_tune_lam2(lower=50, upper=3000, lam1=VISTA_1906.sv2, metric = 'rmse',
                   init_points=10, n_iter=50,verbose = 0,random_state=0)
-VISTA_1906.tune_lam1_parallel(tune_start, 50,
+VISTA_1906.validation_tune_lam1_parallel(tune_start, 50,
                                 VISTA_1906.lam2_opt)
 if tune_start == 2:
     lam_results = VISTA_1906.lam2_opt + VISTA_1906.lam1_opt
@@ -40,9 +40,9 @@ else: lam_results = VISTA_1906.lam1_opt
 
 
 VISTA_1906 = TEC_data(dates, preprocess='quantile',r=5, k=k, missing_rate = 0.5, cutted = True)
-VISTA_1906.tune_lam2(lower=50, upper=3000, lam1=VISTA_1906.sv2, metric = 'rmse',
+VISTA_1906.validation_tune_lam2(lower=50, upper=3000, lam1=VISTA_1906.sv2, metric = 'rmse',
                   init_points=10, n_iter=50,verbose = 0,random_state=0)
-VISTA_1906.tune_lam1_parallel(tune_start, 50,
+VISTA_1906.validation_tune_lam1_parallel(tune_start, 50,
                                 VISTA_1906.lam2_opt)
 
 
@@ -51,5 +51,5 @@ if tune_start == 2:
 else: lam_results_quantile = VISTA_1906.lam1_opt
 
 import json, pathlib
-pathlib.Path(f"lams_{k}_{tune_start}.json").write_text(json.dumps({"lam_results": lam_results, 
+pathlib.Path(f"val_lams_{k}_{tune_start}.json").write_text(json.dumps({"lam_results": lam_results, 
                                                                    "lam_results_quantile": lam_results_quantile}))
