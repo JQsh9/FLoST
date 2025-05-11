47c2438701702641b424d7256059586b966be3f4
Jiuqian Shang <jiuqian@stat-jiuqian-mba.local>
2025-05-11 00:04:14 -0400

    no r restrict

--- job_flost.py
+++ job_flost.py
@@ -25,7 +25,7 @@ from TEC_class import TEC_data
 import sys
 #k = int(sys.argv[1])
 tune_start = int(sys.argv[1])
-k=400
+k=100
 
 dates = ['190621', '190622', '190623', '190624', '190625', '190626']
 
@@ -38,18 +38,20 @@ if tune_start == 2:
     lam_results = VISTA_1906.lam2_opt + VISTA_1906.lam1_opt
 else: lam_results = VISTA_1906.lam1_opt
 
-
+'''
 VISTA_1906 = TEC_data(dates, preprocess='quantile',r=5, k=k, missing_rate = 0.5, cutted = True)
 VISTA_1906.validation_tune_lam2(lower=50, upper=3000, lam1=VISTA_1906.sv2, metric = 'rmse',
                   init_points=10, n_iter=50,verbose = 0,random_state=0)
 VISTA_1906.validation_tune_lam1_parallel(tune_start, 50,
                                 VISTA_1906.lam2_opt)
-
-
-if tune_start == 2:
-    lam_results_quantile = VISTA_1906.lam2_opt + VISTA_1906.lam1_opt
+                                if tune_start == 2:
+lam_results_quantile = VISTA_1906.lam2_opt + VISTA_1906.lam1_opt
 else: lam_results_quantile = VISTA_1906.lam1_opt
-
 import json, pathlib
-pathlib.Path(f"val_lams_{k}_{tune_start}.json").write_text(json.dumps({"lam_results": lam_results, 
-                                                                   "lam_results_quantile": lam_results_quantile}))
+pathlib.Path(f"lams_{k}_{tune_start}.json").write_text(json.dumps({"lam_results": lam_results, 
+                                                                   "lam_results_quantile": lam_results_quantile
+                                                                   }))
+'''
+
+import json
+with open(f"lams_{k}_{tune_start}.json","w") as f: json.dump(lam_results, f)
