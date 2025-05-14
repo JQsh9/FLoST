c7e4448866be103b5216865cf9a3e8373ca8d9f3
Jiuqian Shang <jiuqian@stat-jiuqian-mba.local>
2025-05-13 23:08:10 -0400

    A2

--- job_simu.py
+++ job_simu.py
@@ -26,7 +26,7 @@ def write_simulation_dict(simulation_dict, T,K,p,seed,
     return simulation_dict
 
 T=500
-K=int(T/20)
+K=int(T/5)
 missing=0.5
 d1, d2 = 100,100
 T0 = generate_tensor_simulated(d1, d2, T, k = 10, s=0.2,r=5)
@@ -44,7 +44,7 @@ for my_seed in tqdm(range(0,100)):
     simulation_dict = write_simulation_dict(simulation_dict, T, K, missing, my_seed,'FLT',
                                             A_T100.te_rmse_flt, A_T100.tr_rmse_flt, A_T100.time_flt,
                                             )
-jobname = 'B2'
+jobname = 'A2'
 import json
 with open(jobname+'.json', "w") as fp:
     json.dump(simulation_dict, fp) 
