9ee1bf45482d49740d603b2ecef2f77198ec0d9d
Jiuqian Shang <jiuqian@stat-jiuqian-mba.local>
2025-05-15 13:58:20 -0400

    short_SB1 fixed

--- job.sh
+++ job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ## sbatch job.sh to run
-#SBATCH --job-name=short_SB2
+#SBATCH --job-name=short_SB1
 #SBATCH --mail-user=jiuqian@umich.edu
 #SBATCH --mail-type=FAIL
 
@@ -16,7 +16,7 @@
 
 ## wall time hours:minutes:seconds
 #SBATCH --time=00:30:00
-#SBATCH --array=0-100
+#SBATCH --array=20-100
 
 ###   Load software modules
 ####  Commands your job should run follow this line
