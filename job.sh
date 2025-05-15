48cfd9ba44198c2fa82d1e1929b6541241ba4161
Jiuqian Shang <jiuqian@stat-jiuqian-mba.local>
2025-05-15 16:01:00 -0400

    SpA3 fix

--- job.sh
+++ job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ## sbatch job.sh to run
-#SBATCH --job-name=SpA1
+#SBATCH --job-name=SpA3
 #SBATCH --mail-user=jiuqian@umich.edu
 #SBATCH --mail-type=FAIL
 
@@ -16,7 +16,7 @@
 
 ## wall time hours:minutes:seconds
 #SBATCH --time=01:00:00
-#SBATCH --array=0-60
+#SBATCH --array=40-100
 
 ###   Load software modules
 ####  Commands your job should run follow this line
