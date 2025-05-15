e5e970578479b6f9420dad183f36bd75d2476d4b
Jiuqian Shang <jiuqian@stat-jiuqian-mba.local>
2025-05-15 11:53:15 -0400

    SpB3

--- job.sh
+++ job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ## sbatch job.sh to run
-#SBATCH --job-name=SpA3
+#SBATCH --job-name=SpB3
 #SBATCH --mail-user=jiuqian@umich.edu
 #SBATCH --mail-type=FAIL
 
--- job_simu.py
+++ job_simu.py
@@ -27,9 +27,9 @@ def write_simulation_dict(simulation_dict, T,K,p,seed,
 
 seed_start = int(sys.argv[1])
 '''Change the parameters here'''
-jobname = f'SpA3_{seed_start}'
+jobname = f'SpB3_{seed_start}'
 T=1000
-K=int(T/10)
+K=int(T/20)
 missing=0.5
 s=0.1
 
