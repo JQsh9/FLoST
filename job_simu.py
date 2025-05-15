5f1531dbce64d1d5a7508e5d04db3c1b9bf51902
Jiuqian Shang <jiuqian@stat-jiuqian-mba.local>
2025-05-15 11:56:48 -0400

    SpB1

--- job_simu.py
+++ job_simu.py
@@ -27,9 +27,9 @@ def write_simulation_dict(simulation_dict, T,K,p,seed,
 
 seed_start = int(sys.argv[1])
 '''Change the parameters here'''
-jobname = f'SpA1_{seed_start}'
+jobname = f'SpB1_{seed_start}'
 T=100
-K=int(T/10)
+K=int(T/20)
 missing=0.5
 s=0.1
 
